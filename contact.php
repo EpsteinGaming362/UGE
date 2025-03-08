d10e727f698707657bb09d8e61b4dfc63ebd0529
EpsteinGaming362 <162656335+EpsteinGaming362@users.noreply.github.com>
2025-03-07 21:36:07 -0800

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>Contact Us!</title>
+    </head>
+    <body>
+        <a href="/index.php" class="logo"><img class="logo" src="imgs/UGEhref.png"></a>
+        <h1>How to contact the admins</h1>
+        <br>
+        <br>
+        <div>
+        <p style='width: 20%; position: fixed; top: 70%; left: 40%;'>
+            You can use the form above this text block to report problems or submit ideas for review. You will get an email back from <a href='mailto:unblockedgameengine@gmail.com'>unblockedgameengine@gmail.com</a> after your form has been reviewed.
+            <br>
+            <br>
+            
+            <br>
+            <br>
+            DO NOT report blocked sites; I don't care.
+        </p>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeikoERELn_rLcYnVkefr1uXStel0Grj-or8b3flBlwVy11cA/viewform?embedded=true" width="640" height="484" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
+    </body>
+    <style>
+        h1{
+            width: 100%;
+        }
+        *{
+            font-family: "Lucida Console", "Courier New", monospace;
+            text-align: center;
+        }
+        .logo{
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            width: 10%;
+        }
+    </style>
+</html>
